071151d8edb516f75e9a14d1d84aa9cc72eb454c
SeuFernandez <64269128+seufernandez@users.noreply.github.com>
2021-10-19 09:12:44 -0300

    update: theme color meta tag added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="theme-color" content="#171923">
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
